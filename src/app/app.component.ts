3cee2c7cf6fcf5837bcf08fcf24e8d2830adad13
snehal patel <snpatel.dev@gmail.com>
2022-12-19 13:29:33 -0500

    #1 remove todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Todos } from './todos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Todo App';
+  todoName!: string;
+  errMessage!: boolean;
+  todos: Todos[] = [
+    {
+      name: 'learn Javascript',
+      isCompleted: false,
+    },
+    {
+      name: 'learn Angular',
+      isCompleted: false,
+    },
+  ];
+
+  onAdd() {
+    if (!this.todoName) return;
+    this.todos.push({ name: this.todoName, isCompleted: false });
+    this.todoName = '';
+  }
+
+  onDelete(event: Event) {
+    const todo = event.target as HTMLInputElement;
+    const parent = todo.parentElement?.parentElement;
+    parent?.remove();
+  }
 }
